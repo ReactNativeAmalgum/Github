304ddb9266e5aef4dbb9d83fa9863f46b1e518d2
ReactNativeAmalgum <reactnativeamalgum@gmai.com>
2025-06-17 20:59:24 +0530

    banana

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload. new
-          new line here
+          new line here, banana
         </p>
         <h1>
           Git ammend, now ammended without again commiting
