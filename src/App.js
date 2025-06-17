5cf0dfd1a8ff5c4b4ee254ef6fe23101019cc75f
ReactNativeAmalgum <reactnativeamalgum@gmai.com>
2025-06-17 19:59:29 +0530

    To be amend, now amended

--- src/App.js
+++ src/App.js
@@ -9,6 +9,9 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        <h1>
+          Git ammend, now ammended without again commiting
+        </h1>
         <a
           className="App-link"
           href="https://reactjs.org"
