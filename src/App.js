590c47daa613d1ec31ce5abcaa64dd18ea0240fe
ReactNativeAmalgum <reactnativeamalgum@gmai.com>
2025-06-17 20:58:23 +0530

    new change

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload. new
-          new line here
+          new line here water
         </p>
         <a
           className="App-link"
