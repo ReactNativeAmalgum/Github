b325a55f033d67f871afa4cecaf445cccb24f513
ReactNativeAmalgum <reactnativeamalgum@gmai.com>
2025-06-18 20:33:01 +0530

    Initial clean commit

---
+++ src/hell.js
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const hell = () => {
+  return (
+    <View>
+      <Text>hell000000</Text>
+    </View>
+  )
+}
+
+export default hell
